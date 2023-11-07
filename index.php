690bbc4d87858d144968781293c9fa32b13c5f9e
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2023-11-07 12:43:03 +0900

    変更

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<?php require 'header/header.php'; ?>
+<?php require 'header/header-b.php'; ?>
 <?php require 'menu-top.php'; ?>
-<?php require 'footer/footer.php'; ?>
+<?php require 'footer/footer-v.php'; ?>
