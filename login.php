ed31086b03d1e954b37fb168fa1cf59b3dce6dd7
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2023-11-08 12:02:37 +0900

    コミット

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン</title>
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+    <div>
+<form action="../alcoholdrink/Top.php" method="post">
+    <p>アカウントID</p>
+    <input type="text" name="login"><br>
+    <p>パスワード</p>
+    <input type="text" name="password"><br>
+    <input type="submit" name="logina" value="ログイン">
+    <p>アカウント新規作成は<a href=../account-registration.php>こちら</a></p>
+</div>
+</form>
+</body>
+</html>
