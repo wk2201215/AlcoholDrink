12d130b103305f0020d9ff068ff28793ab7f5c54
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2023-11-14 12:09:23 +0900

    input終わった？

--- costomer-output.php
+++ costomer-output.php
@@ -9,28 +9,40 @@ if(isset($_SESSION['customer'])){
     $id=$_SESSION['customer']['id'];
     $sql=$pdo->prepare('select * from Customers where id!=? and login=?');
     $sql->execute([$id,$_POST['login']]);
-} else {
-    $sql=$pdo->prepare('select * from Customers where id!=? and login=?');
-    $sql->execute([$id,$_POST['login']]); 
 }else{
     $sql=$pdo->prepare('select * from Customers where login=?');
     $sql->execute([$_POST['login']]);
 }
 if(empty($sql->fetchAll())) {
     if(isset($_SESSION['customer'])){
-        $sql=$pdo->prepare('update  Customers set login_id customer_name customer_password postcode address telephone mail birth  where id=?');
+        $sql=$pdo->prepare('update  Customers set login_id 
+        customer_name customer_password postcode address 
+        telephone mail birth where id=?');
         $sql->execute([
-            $_POST['name'],$_POST['address'],
-            $_POST['login'],$pass,$id]);
+            $_POST['id'],$_POST['name'],
+            $_POST['password'],$_POST['zipcode'],
+            $_POST['address'],$_POST['tel'],
+            $_POST['mail'],$_POST['dirthdate'],
+            $id]);
         $_SESSION['customer']=[
-            'id'=>$id, 'name'=>$_POST['name'],'address'=>$_POST['address'],
-            'login'=>$_POST['login'],'password'=>$pass];
+            'id'=>$id, 'name'=>$_POST['name'],
+            'password'=>$_POST['password'], 
+            'zipcode'=>$_POST['zipcode'],
+            'address'=>$_POST['address'], 
+            'tel'=>$_POST['tel'],'mail'=>$_POST['mail'],
+            'dirthdate'=>$_POST['dirthdate'],
+            'login'=>$_POST['login']];
             echo 'お客様情報を更新しました。';
     } else {
-        $sql=$pdo->prepare('insert into customer value(null,?,?,?,?)');
+        $image = uniqid(mt_rand(), true);//ファイル名をユニーク化
+        $image .= '.' . substr(strrchr($_FILES['image']['name'], '.'), 1);//アップロードされたファイルの拡張子を取得
+        $file = "images/$image";
+        $sql=$pdo->prepare('insert into Customer value(null,?,?,?,?,?,?,?,?,?,null,null,null,null,null)');
         $sql->execute([
-            $_POST['name'],$_POST['address'],
-            $_POST['login'],$pass]);
+            $_POST['id'],$_POST['name'],
+            $_POST['password'],$_POST['zipcode'],
+            $_POST['address'],$_POST['tel'],
+            $_POST['mail'],$_POST['dirthdate']]);
         echo 'お客様情報を登録しました。';
     }
 } else {
