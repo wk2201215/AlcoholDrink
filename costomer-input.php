8a0e849fbcdb1049415010fe41937719af38ff92
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2023-11-15 14:03:34 +0900

    顧客登録終わった

--- costomer-input.php
+++ costomer-input.php
@@ -3,7 +3,7 @@
 <?php require 'header.php'; ?>
 <?php require 'menu.php'; ?>
 <?php
-    $login_id=$name=$password=$postcode=$address=$tel=$mail=$birth=$idcard='';
+    $login_id=$name=$password=$postcode=$address=$tel=$mail=$birth='';
     if(isset($_SESSION['customer'])){
         $login_id=$_SESSION['customer']['login_id'];
         $name=$_SESSION['customer']['name'];
@@ -13,36 +13,36 @@
         $tel=$_SESSION['customer']['tel'];
         $mail=$_SESSION['customer']['mail'];
         $birth=$_SESSION['customer']['birth'];
-        $idcard=$_SESSION['customer']['idcard'];
+        // $idcard=$_SESSION['customer']['idcard'];
     }
-    echo '<form action="costomer-output.php" method="post">';
+    echo '<form action="costomer-output.php" method="post" enctype="multipart/form-data">';
     echo '<table>';
     echo '<tr><td>アカウントID</td><td><br>';
-    echo '<input type="text" name="login_id"  value="', $login_id, '" >';
+    echo '<input type="text" name="login_id"  value="', $login_id, '" required>';
     echo '</td></tr>';
     echo '<tr><td>氏名</td><td><br>';
-    echo '<input type="text" name="name"  value="', $name, '" >';
+    echo '<input type="text" name="name"  value="', $name, '" required>';
     echo '</td></tr>';
     echo '<tr><td>パスワード</td><td><br>';
-    echo '<input type="password" name="password"  value="', $password, '" >';
+    echo '<input type="password" name="password"  value="', $password, '" required>';
     echo '</td></tr>';
     echo '<tr><td>郵便番号</td><td><br>';
-    echo '<input type="text" name="postcode"  value="', $postcode, '" >';
+    echo '<input type="text" name="postcode"  value="', $postcode, '" required>';
     echo '</td></tr>';
     echo '<tr><td>住所</td><td><br>';
-    echo '<input type="text" name="address"  value="', $address, '" >';
+    echo '<input type="text" name="address"  value="', $address, '" required>';
     echo '</td></tr>';
     echo '<tr><td>電話番号</td><td><br>';
-    echo '<input type="text" name="tel"  value="', $tel, '" >';
+    echo '<input type="text" name="tel"  value="', $tel, '" required>';
     echo '</td></tr>';
     echo '<tr><td>メールアドレス</td><td><br>';
-    echo '<input type="text" name="mail"  value="', $mail, '" >';
+    echo '<input type="text" name="mail"  value="', $mail, '" required>';
     echo '</td></tr>';
     echo '<tr><td>生年月日</td><td><br>';
-    echo '<input type="date" name="dirth"  value="', $birth, '" >';
+    echo '<input type="date" name="dirth"  value="', $birth, '" required>';
     echo '</td></tr>';
     echo '<tr><td>身分証など本人確認ができる写真をお願いします</td><td><br>';
-    echo '<input type="file" accept="image/*" name="idcard"  value="', $idcard, '" >';
+    echo '<input type="file" accept="image/*" name="idcard" required>';
     echo '</td></tr>';
     echo '</table>';
     echo '<input type="submit"  value="登録">';
--- db-connect.php
+++ db-connect.php
@@ -1,8 +1,8 @@
 <?php
     const SERVER = 'mysql219.phy.lolipop.lan';
-    const DBNAME = 'LAA1517449-alcoholdrink';
-    const USER = 'LAA1517449';
-    const PASS = 'Pass1036';
+    const DBNAME = 'LAA1517442-alcoholdrink';
+    const USER = 'LAA1517442';
+    const PASS = 'Pass1106';
 
     $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
 ?>
