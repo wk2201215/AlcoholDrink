bbef6ca54136a69329f5ebd2daeab36793ddc917
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2023-11-28 15:02:54 +0900

    komittto

--- purchase-input.php
+++ purchase-input.php
@@ -1,21 +1,31 @@
 <?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
+<?php require 'header-menu.php'; ?>
+
 <?php
-if(isset($_SESSION['customer'])){
-if(!empty($_SESSION['product'])){
-    echo '<p>お名前：',$_SESSION['customer']['name'],'</p>';
-    echo '<p>ご住所：',$_SESSION['customer']['address'],'</p>';
-    echo '<hr>';
-    require 'cart.php';
-    echo '<hr>';
-    echo '<a href="purchase-output.php">購入を確定する</a>';
-}else{
-    echo 'カートに商品がありません。';
-}
-}else{
-    echo 'ログインしてください。';
-}
-?>
+    
+    $w=0.1;
+    $discount=$_SESSION['cart']['product']['id']['price']*$w;
+    $fee=100;
+    $total=$_SESSION['cart']['total']-$discount+$fee;
+    echo '<p>ご請求金額',$total,'</p>';
+    echo '<p>小計',$_SESSION['cart']['total'],'</p>';
+    echo '<p>割引',$discount,'円(',$w*100,'%)</p>';
+    echo '<p>配送料・手数料',$fee,'</p>';
+
+    echo 'お届け先';
+    
+    echo $SESIION['cusotmer']['address'];
+    ?>
+    <button type="button" onclick="location.href='customer-input.php'">住所変更</button>
+<?php
+    echo '支払い方法';
+    
+    echo $SESIION['cusotmer']['payment'];
+    ?>
+    <button type="button" onclick="location.href='customer-payment-input.php'">支払い方法を変更</button>
+    <button type="button" onclick="location.href='purchase-output.php'">購入を確定する</button>
+
+<?php require 'footer-menu.php'; ?>
 <?php require 'footer.php'; ?>
--- purchase-output.php
+++ purchase-output.php
@@ -1,21 +1,36 @@
 <?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
-<?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
 <?php
-if(isset($_SESSION['customer'])){
     $pdo=new PDO($connect, USER, PASS);
-    $sql=$pdo->prepare('insert into Purchase values(null,?)');
+    $sql=$pdo->prepare('insert into Orders values(null,?,null,?,?)');
     // var_dump($_SESSION['customer']['id']);
-    $sql->execute([$_SESSION['customer']['id']]); 
+    $sql->execute([
+        $_SESSION['customer']['id'], 
+        $_SESSION['customer']['address'], 
+        $_SESSION['customer']['payment']    
+    ]); 
     $last_id = $pdo->lastInsertId();
-    $sql2=$pdo->prepare('insert into Purchase_detail values(?,?,?)');
-    foreach($_SESSION['product'] as $id=>$product){
+    $sql2=$pdo->prepare('insert into Order_details values(?,?,?)');
+    foreach($_SESSION['cart']['puroduct'] as $id=>$product){
         // $sql2=$pdo->prepare('insert into Purchase_detail values(?,?,?)');
         $sql2->execute([$last_id, $id, $product['count']]);
-        unset($_SESSION['product'][$id]);
+        $sql3=$pdo->prepare('update  products set stock=?  where product_id=?');    
+        $sql3->execute([
+            $SESSION['cart']['puroduct']['']            
+            $SESSION['cart']['puroduct']['id']
+        ]);
     }
-} 
+    
+    $sql=$pdo->prepare('delete from Orders  where order_id=?');  
+    $sql->execute([
+        $_SESSION['cart']['id'], 
+    ]); 
+
+    
+    $sql=$pdo->prepare('delete from Order_details where order_id=?');    
+    $sql->execute([
+        $_SESSION['cart']['id'], 
+    ]); 
+
+    unset($_SESSION['cart']);
 ?>
-<p>購入手続きが完了しました。ありがとうございます。
-<?php require 'footer.php'; ?>
