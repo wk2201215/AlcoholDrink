439be0850ed21969bf907b843aa05be967fe1f4e
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2023-11-28 12:35:24 +0900

    ログイン

--- login-output.php
+++ login-output.php
@@ -1,22 +1,12 @@
-<?php session_start();?>
+<?php session_start(); ?>
 <?php require 'db-connect.php';?>
-<?php require 'header.php';?>
-<?php require 'header-menu.php'; ?>
 <?php
 unset($_SESSION['customer']);
 $pdo=new PDO($connect, USER, PASS);
 $sql=$pdo->prepare('select * from Customers where login_id=?');
-// $pass=password_hash($_POST['password'], PASSWORD_DEFAULT);
 $sql->execute([$_POST['login_id']]);
-// var_dump($_POST);
-// $k=$sql->fetchAll();
-// var_dump($k);
-foreach ($sql as $row){
-    // echo 1;
-    // $pass=password_hash($_POST['password'], PASSWORD_DEFAULT);
-    // echo $pass;
+foreach($sql as $row) {
     if(password_verify($_POST['password'],$row['customer_password'])){
-        // echo '1';
         $_SESSION['customer']=[
             'id'=>$row['customer_id'],
             'login_id'=>$row['login_id'],
@@ -28,19 +18,15 @@ foreach ($sql as $row){
             'mail'=>$row['mail'],
             'birth'=>$row['birth'],
             'payment'=>$row['customer_payment']
+
         ];
     }
 }
-// var_dump($_SESSION);
 if(isset($_SESSION['customer'])){
-    echo 'いらっしゃいませ、', $_SESSION['customer']['name'], 'さん。';
-    // echo '<button type="button" onclick="location.href='top.php'">トtop.phpップページへ</button>';
-    // var_dump($_SESSION['customer']['id']);
+header('Location:top.php');
+exit();    
 }else{
-    echo 'ログイン名またはパスワードが違います。';
-    echo'<button type="button" onclick=history.back()>戻る</button>';
-    // echo $pass;
+header('Location:login.php?hogeA=ログイン名またはパスワードが違います');
+exit();
 }
 ?>
-<?php require 'footer-menu.php'; ?>
-<?php require 'footer.php';?>
