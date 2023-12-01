a5fb47ef7c4e9a1e36b3e21b2eb5cfc047fee32f
K-Shinhira <144861653+K-Shinhira@users.noreply.github.com>
2023-12-01 09:41:12 +0900

    jk

--- login.php
+++ login.php
@@ -7,23 +7,23 @@
     }
 ?>
 <?php
-echo '<div class="field">';
-        echo '<label class="label">アカウントID</label>';
-        echo '<input type="text" class="input" name="login_id"><br>';
-        echo  '</div>';
+    echo '<div class="field">';
+    echo '<label class="label">アカウントID</label>';
+    echo '<input type="text" class="input" name="login_id">';
+    echo '</div>';
 
-        echo '<div class="field">';
-        echo '<label class="label">パスワード</label>';
-        echo  '<input type="password" class="input" name="password"><br>';
-        echo '</div>';
+    echo '<div class="field">';
+    echo '<label class="label">パスワード</label>';
+    echo '<input type="password" class="input" name="password">';
+    echo '</div>';
 
-        echo '<div class="field">';    
-        echo  '<input type="submit" class=" button mx-auto " value="ログイン">';
-        echo '</div>';
+    echo '<div class="field">';    
+    echo '<input type="submit" class=" button mx-auto " value="ログイン">';
+    echo '</div>';
         
-        echo '<div class="field">';
-        echo  '<label>アカウント新規作成は<a href="customer-input.php">こちら</a></label>';
-        echo '<div class="field">';
+    echo '<div class="field">';
+    echo '<label>アカウント新規作成は<a href="customer-input.php">こちら</a></label>';
+    echo '</div>';
 ?>
 
 <?php require 'footer.php'; ?>
