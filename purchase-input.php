1b49e157afd7748cc3dca1b24d5c95d96e70169d
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2023-12-05 14:34:40 +0900

    input できた

--- purchase-input.php
+++ purchase-input.php
@@ -15,26 +15,27 @@
     $wari=[
         'name'=>[],
         'd'=>[],
-        'dp'=>[]
+        'dp'=>[],
+        'c'=>[]
     ];
-    $i=1;
+    $n=0;
     foreach ($sql as $row){
         $sum+=$row["cart_quantity"]*$row["price"];
         if($row["discount"] != 0){
         $discount+=$row['price']*($row["discount"]/100)*$row['cart_quantity'];
-        $wari['name'][$i] = $row['product_name'];
-        $wari['d'][$i] = $row['discount'];
-        $wari['dp'][$i] = $row['price']*($row["discount"]/100)*$row['cart_quantity'];
-        $i++;
+        $wari['name'][$n] = $row['product_name'];
+        $wari['d'][$n] = $row['discount'];
+        $wari['dp'][$n] = $row['price']*($row["discount"]/100);
+        $wari['c'][$n] = $row['cart_quantity'];
+        $n++;
         }
     }
-
     $fee=100;
     $total=$sum-$discount+$fee;
-    echo '<p>ご請求金額',$total,'</p>';
+    echo '<p>ご請求金額',round($total),'</p>';
     echo '<p>小計',$sum,'</p>';
-    foreach($wari as $row){
-    echo '<p>割引',$row['name'],$row['dp'],'円(',$row['d'],'%)</p>';
+    for( $i=0;$i<$n;$i++){
+    echo '<p>割引',$wari['name'][$i],$wari['dp'][$i],'円(',$wari['d'][$i],'%)×',$wari['c'][$i],'</p>';
     }
     echo '<p>配送料・手数料',$fee,'円','</p>';
 
