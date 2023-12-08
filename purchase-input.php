ea38e65008b67123af8655e64f850c967203452e
Nozakiishin <148302881+Nozakiishin@users.noreply.github.com>
2023-12-08 12:45:46 +0900

    henkou

--- purchase-input.php
+++ purchase-input.php
@@ -41,13 +41,13 @@
 
     echo 'お届け先';
     
-    echo $SESIION['cusotmer']['address'];
+    echo $_SESIION['cusotmer']['address'];
     ?>
     <button type="button" onclick="location.href='customer-input.php'">住所変更</button><br>
 <?php
     echo '支払い方法';
     
-    echo $SESIION['cusotmer']['payment'];
+    echo $_SESIION['cusotmer']['payment'];
     ?>
     <button type="button" onclick="location.href='customer-payment-input.php'">支払い方法を変更</button><br>
     <button type="button" onclick="location.href='purchase-process.php'">購入を確定する</button>
