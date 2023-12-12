bc0cbdca73b85fbcb1ab6e7e926bb409355ab6e6
TanimotoAmi <148303586+TanimotoAmi@users.noreply.github.com>
2023-12-12 14:41:50 +0900

    henkou

--- kannrisya/login-input.php
+++ kannrisya/login-input.php
@@ -1,9 +1,31 @@
 <?php require 'header.php'; ?>
 
 <form action="login-output.php" method="post">
-    ログイン名<input type = "text" name = "name"><br>
-    パスワード<input type = "password" name = "password"><br>
-    <input type = "submit" value = "ログイン">
+<?php
+    unset($_SESSION['control']);
+    echo '<div class="field">';
+    echo '<label class="label">管理者名</label>';
+    echo '<input type="text" class="input" name="name">';
+    echo '</div>';
+
+    echo '<div class="field">';
+    echo '<label class="label">パスワード</label>';
+    echo '<input type="password" class="input" name="password">';
+    echo '</div>';
+
+    echo '<div class="field">';
+    echo '<p class="has-text-centered">';    
+    echo '<input type="submit" class="button mx-auto is-link" value="ログイン">';
+    echo '</p>';
+    echo '</div>';
+
+    if(isset($_GET['hogeA'])){
+        echo '<p class="has-text-danger">',$_GET['hogeA'],'</p>';
+    }else{
+        echo '<p class="has-text-danger"><br></p>';
+    }
+?>
+
 </form>
 
 <?php require 'footer.php'; ?>
