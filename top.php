fcc1f81f63895116e1eacd3e20612bae9e796b66
K-Shinhira <144861653+K-Shinhira@users.noreply.github.com>
2023-12-12 17:43:10 +0900

    henkou

--- top.php
+++ top.php
@@ -74,14 +74,14 @@ foreach ($sql as $i => $row) {
    $id=$row['product_id'];
    echo '<div style="width:50vw;">'; 
    echo '<div class="box p-0 m-1">';
-   echo '<figure class="image is-fullwidth has-background-white-ter full_image py-5" style="height:35vh">';
+   echo '<figure class="image is-fullwidth has-background-white-ter full_image py-5 my-0" style="height:35vh">';
    echo '<a class="center_image" href="detail.php?id=',$id,'"><img alt="images" src="images/products/',$row['image_pass'],'"></a>';
    echo '</figure>';
    echo '<div style="height:calc(3rem * 1.5)">';
-   echo '<a class="txt-limit3" style="text-decoration:none;">',$row['product_name'],'</a>';
+   echo '<a class="txt-limit3" href="detail.php?id=',$id,'" style="text-decoration:none;">',$row['product_name'],'</a>';
    echo '</div>';
    echo '<div class="py-3">';
-   echo '<p class="subtitle txt-limit2 has-text-danger py-0">￥',number_format($row['price']),'</p>';
+   echo '<p class="subtitle txt-limit2 has-text-danger py-0 my-0">￥',number_format($row['price']),'</p>';
    echo '</div>';
    echo '</div>';
    echo '</div>';
