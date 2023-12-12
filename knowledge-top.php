763c6c255e716f4468663fdf78e5d7ceb135a292
K-Shinhira <144861653+K-Shinhira@users.noreply.github.com>
2023-12-12 11:20:49 +0900

    henkou

--- knowledge-top.php
+++ knowledge-top.php
@@ -5,17 +5,16 @@
 <?php require 'header-menu.php'; ?>
 <div class="hero-body py-5">
 <?php
-    $pdo1=new PDO($connect, USER, PASS);
-    $pdo2=new PDO($connect, USER, PASS);
+    $pdo=new PDO($connect, USER, PASS);
     echo '<h1>お酒の知識</h1>';
-    foreach($pdo1->query('select * from Knowledge') as $row1){
+    foreach($pdo->query('select * from Knowledge') as $row1){
         echo '<div class="box is-clipped">';
         echo '<label class="label txt-show">', $row1['knowledge_name'],'</label>';
         echo '<div class="contents-hide">';
         echo '<p>', $row1['knowledge_text'],'</p>';
         echo '<p>関連商品</p>';
         echo '<figure class="mt-3">';
-        $sql=$pdo2->prepare('
+        $sql=$pdo->prepare('
         select A.product_id,image_pass from Knowledge_products as A 
         inner join Products as B 
         On A.product_id=B.product_id 
