324904947ee4ca161f162bf5e65b0017b4e7abed
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2023-12-13 12:02:01 +0900

    akukinn

--- top.php
+++ top.php
@@ -1,12 +1,4 @@
-<?php
-// リファラーを取得
-$referer = @$_SERVER['HTTP_REFERER'];
-
-// リファラーが存在しない場合、直接アクセスとみなしエラーメッセージを表示
-if (empty($referer)) {
-    die('このページへの直接アクセスは禁止されています。');
-}
-?>
+<?php require 'not-access.php'; ?>
 <?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
 <?php require 'header-top.php'; ?>
