a76d9e719721b74e95c9eb8607a5f64be1167ac6
K-Shinhira <144861653+K-Shinhira@users.noreply.github.com>
2023-12-13 16:17:14 +0900

    henkou

--- top-s.php
+++ top-s.php
@@ -6,7 +6,7 @@
     $count=0;
     foreach($pdo2->query('select * from Slide') as $row){
       echo '<div>';
-      echo '<img src="'.$row['img_pas'].'">';
+      echo '<img src="images/slide/'.$row['img_pas'].'">';
       echo '</div>';
       $count++;
     }
